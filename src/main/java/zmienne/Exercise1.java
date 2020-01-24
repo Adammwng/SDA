08cc169d853992e259a768da7e1778883f384073
mnwg <adammnwg@gmail.com>
2020-01-24 17:23:25 +0100

    check

---
+++ src/main/java/zmienne/Exercise1.java
@@ -0,0 +1,16 @@
+package zmienne;
+
+public class Exercise1 {
+    //Zadeklaruj trzy zmienne liczbowe (całkowite) i przypisz im wartości.
+    //Następnie wypisz wartość największej z nich.
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 11;
+        int z = 8;
+        if (x>y && x >y){
+            System.out.println(x);
+        }else if (y>x && y >z){
+            System.out.println(y);
+        }else System.out.println(z);
+    }
+}
