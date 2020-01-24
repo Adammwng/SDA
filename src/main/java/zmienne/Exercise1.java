9684a5bc3610840a6f4a71ce8241d9b29ac4d9c1
mnwg <adammnwg@gmail.com>
2020-01-24 20:38:04 +0100

    add file

--- src/main/java/zmienne/Exercise1.java
+++ src/main/java/zmienne/Exercise1.java
@@ -6,7 +6,7 @@ public class Exercise1 {
     public static void main(String[] args) {
         int x = 10;
         int y = 11;
-        int z = 8;
+        int z = 232;
         if (x>y && x >y){
             System.out.println(x);
         }else if (y>x && y >z){
